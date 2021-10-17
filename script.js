01972b1d6f2d45546af73e3903b844791247556e
faertan <ilhanasimfurkan@gmail.com>
2021-10-17 17:40:40 +0300

    about and discordbots page

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function instagrambutton(){
-    
+const google_logo = document.getElementById("about");
+
+google_logo.onclick = () => {
+    window.scrollBy({
+        top: 800,
+        left: 0,
+        behavior: "smooth"
+    });
 }
